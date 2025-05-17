cfad32eb71b85ccffa472f3d06044220f648b71b
Eduardo <93397077+Eduard38655@users.noreply.github.com>
2025-05-17 11:23:34 -0400

    Update Email_Message.js

--- BanckedControllers/Email_Message.js
+++ BanckedControllers/Email_Message.js
@@ -1,7 +1,6 @@
- 
-import cors from "cors";
-import "dotenv/config";
+
 import express from "express";
+import cors from "cors";
 import nodemailer from "nodemailer";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -9,49 +8,49 @@ import { fileURLToPath } from "url";
 const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
- 
-let Allow=["https://eduard38655.github.io","http://localhost:5173"]
-router.use(cors({ 
-  credentials: true, 
-  origin: Allow,
-  exposedHeaders: ['set-cookie'] 
-}));
 
+const allowedOrigins = [
+  "https://eduard38655.github.io",
+  "http://localhost:5173",
+];
+
+router.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
- 
 router.use(express.static(path.join(__dirname, "public")));
- 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user:PROCESS.env.USER,
-      pass:PROCESS.env.PASSWORD
-    }
-  });
-  
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASSWORD,
+  },
+});
+
 router.post("/DatosSend/Personal", async (req, res) => {
-    const { FullName, Email, Message } = req.body;
-  
-    
-  
-    const mailOptions = {
-      from: `"${FullName}" <${PROCESS.env.USER}>`,  
-      to: Email,                                     
-      subject: `Repository`,
-      text: Message
-    };
-  
-    try {
-      const info = await transporter.sendMail(mailOptions);  
-      console.log("Email sent:", info.messageId);
-      return res.json({ valid: true, messageId: info.messageId });
-      
-    } catch (error) {
-      console.error("Nodemailer error:", error);
-      return res.status(500).json({ valid: false, error: error.message });
-    }
-  });
+  const { FullName, Email, Message } = req.body;
 
+  const mailOptions = {
+    from: `"${FullName}" <${process.env.USER}>`,
+    to: Email,
+    subject: `Repository`,
+    text: Message,
+  };
 
-export default router
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.messageId);
+    res.json({ valid: true, messageId: info.messageId });
+  } catch (error) {
+    console.error("Nodemailer error:", error);
+    res.status(500).json({ valid: false, error: error.message });
+  }
+});
+
+export default router;
